5e32e567219c56aefc296cbe3922f46ba26c4278
yuanwen <wenshin2011@gmail.com>
2016-09-13 16:39:09 +0800

    Update: when use modifyResponse option, also can bind response event in promise stream

--- test/test-proxy.js
+++ test/test-proxy.js
@@ -94,4 +94,33 @@ describe('proxy-request', function () {
       });
     });
   });
+
+  it('proxy(req, {url, modifyResponse}, res).then(request => request.on("response", fn))', function (done) {
+    const MODIFIED = 'modified';
+    utils.test(function(req, res) {
+      const ctx = this;
+      proxy(req, {
+        url: `http://localhost:${this.address().port}`,
+        modifyResponse(body) {
+          assert.equal(body, ctx.s.successText);
+          assert.equal(res.statusCode, 200);
+          this.response.statusCode = 206;
+          return MODIFIED;
+        }
+      }, res)
+        .then(request => {
+          request.on('response', (response) => {
+            response.headers.test = 'test';
+          });
+        });
+    }, function() {
+      const ctx = this;
+      utils.get.call(ctx, null, function(res, body) {
+        assert.equal(res.statusCode, 206);
+        assert.equal(body, MODIFIED);
+        assert.equal(res.headers.test, 'test');
+        done()
+      });
+    });
+  });
 });
